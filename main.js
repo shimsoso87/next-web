7ad3dc0f55a41f8bd94fce907e6ccc06f60d54ca
shimsoso87 <114768393+shimsoso87@users.noreply.github.com>
2022-10-04 02:24:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const numbers = new Array(1,2,3,45);
+
+console.log(numbers);
+
+const fruits=['apple','mango','kiwi'];
+ 
+fruits.push('grapes');
+fruits.unshift('strawberry');
+fruits.pop;
+console.log(fruits);
+console.log(Array.isArray(fruits));
+console.log(fruits.indexOf('kiwi'));
+
